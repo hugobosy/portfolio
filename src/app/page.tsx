cde0266a80b55e15c803d82343f0a78de2b868b4
Hubert Kliszcz <hubert.kliszcz@gmail.com>
2023-12-19 23:12:25 +0100

    Add typed text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import { Button } from "@/components/base/button/Button";
+import {TypedText} from "@/components/base/typedText/TypedText";
 
 export default function Home() {
   return (
     <main>
       <Button type="button" text="Button" />
+        <TypedText textList={['Developer', 'Designer', 'Programmer']} backSpeed={80} backDelay={1500} typeSpeed={80} cursorChart=""/>
     </main>
   );
 }
---
+++ src/components/base/typedText/TypedText.tsx
@@ -0,0 +1,38 @@
+import {ComponentPropsWithoutRef, FC, useEffect, useRef} from "react";
+import Typed from "typed.js";
+
+import styles from './TypedText.module.scss'
+
+export interface TypedTextProps extends ComponentPropsWithoutRef<'div'> {
+    textList: string[];
+    typeSpeed?: number;
+    backSpeed?: number;
+    backDelay?: number;
+    loop?: boolean;
+    cursorChart?: string;
+}
+
+export const TypedText: FC<TypedTextProps> = ({textList, typeSpeed, backSpeed, backDelay, loop, cursorChart}) => {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: textList,
+            typeSpeed: typeSpeed,
+            backSpeed: backSpeed,
+            backDelay: backDelay,
+            loop: loop,
+            cursorChar: cursorChart,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+    return (
+        <div className={styles.typedCursor}>
+            <h1 style={{display: "inline-block", fontSize: 48}} className={styles.typedCursor} ref={el} />
+        </div>
+    );
+}
